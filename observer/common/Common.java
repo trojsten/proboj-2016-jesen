a9a420a2a826f3e947a842b28da3fefc480540fa
faiface <faiface@ksp.sk>
2016-10-31 17:39:13 +0100

    delete observer

--- observer/common/Common.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class Common {
-	public static int velkyCas;
-	public static int POMALOST_RASTU = 1;
-	public static int TAH_CAS = 20;
-	public static int UTOK_TEMNYCH = 9;
-}
