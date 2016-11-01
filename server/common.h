8e5d0046fcfc5adce1ceba9896eb50da6459078c
faiface <faiface@ksp.sk>
2016-11-01 20:27:11 +0100

    add basic structures

--- server/common.h
+++ server/common.h
@@ -1,6 +1,34 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <vector>
+#include <map>
+
+using namespace std;
+
+struct point {
+    int x, y;
+};
+
+struct block {
+    int color;
+    bool solid;
+};
+
+struct player {
+    int color;
+    point position;
+
+    int score;
+
+    //TODO: bonuses
+};
+
+struct area {
+    vector<player> players;
+    map<point, vector<block>> blocks;
+};
+
 #endif
 
 
