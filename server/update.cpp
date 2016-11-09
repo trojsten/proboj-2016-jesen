a647e8e3759a8144bc213df7cb1dad820306d5bf
faiface <faiface@ksp.sk>
2016-11-09 19:55:07 +0100

    plnenie teritoria

--- server/update.cpp
+++ server/update.cpp
@@ -3,19 +3,72 @@
 
 using namespace std;
 
-void killPlayer (game_state& gs, int dier) {
+game_state kill_player(game_state gs, int dier) {
+    // delete all crossed blocks
     gs.players[dier].alive = false;
-    for (int x = 0; x < gs.width; x++) {
-        for (int y = 0; y < gs.height; y++) {
-            int i = gs.block_index({x, y});
-            if (gs.blocks[i].owned_by == dier) {
-                gs.blocks[i].owned_by = -1;
-            }
-            if (gs.blocks[i].crossed_by == dier) {
-                gs.blocks[i].crossed_by = -1;
-            }
-        }
+    for (unsigned i = 0; i < gs.blocks.size(); i++) {
+	if (gs.blocks[i].crossed_by == dier) {
+	    gs.blocks[i].crossed_by = -1;
+	}
     }
+    return gs;
+}
+
+game_state own_territory(game_state gs, int player) {
+    // mark all crossed blocks as owned
+    for (unsigned i = 0; i < gs.blocks.size(); i++) {
+	if (gs.blocks[i].crossed_by == player) {
+	    gs.blocks[i].owned_by = player;
+	    gs.blocks[i].crossed_by = -1;
+	}
+    }
+
+    vector<bool> visited(gs.blocks.size(), false);
+    for (int x = 0; x < gs.width; x++) {
+	for (int y = 0; y < gs.height; y++) {
+	    int i = gs.block_index({x, y});
+
+	    if (visited[i]) continue;
+
+	    bool should_own = true;
+
+	    vector<pair<int, int>> stack;
+	    vector<bool> tested(gs.blocks.size(), false);
+	    stack.push_back({x, y});
+
+	    while (!stack.empty()) {
+		int x = stack.back().first;
+		int y = stack.back().second;
+		int i = gs.block_index({x, y});
+		stack.pop_back();
+
+		if (gs.blocks[i].owned_by == player) continue;
+
+		if (tested[i]) continue;
+		tested[i] = true;
+		
+		if (x == 0 || y == 0 || x == gs.width - 1 || y == gs.height - 1) {
+		    should_own = false;
+		    break;
+		}
+
+		stack.push_back({x - 1, y});
+		stack.push_back({x + 1, y});
+		stack.push_back({x, y - 1});
+		stack.push_back({x, y + 1});
+	    }
+
+	    if (should_own) {
+		for (unsigned i = 0; i < gs.blocks.size(); i++) {
+		    if (tested[i]) {
+			gs.blocks[i].owned_by = player;
+		    }
+		}
+	    }
+	}
+    }
+
+    return gs;
 }
 
 game_state update_game_state(game_state gs, vector<vector<player_command> > commands) {
@@ -52,31 +105,41 @@ game_state update_game_state(game_state gs, vector<vector<player_command> > comm
             break;
         }
         new_gs.players[i].position = new_position;
-        
+    }
+
+    // zistime, ci sa nezrazili
+    for (unsigned i = 0; i < new_gs.players.size(); i++) {
         // nezabil sa o stenu / okraj mapy?
-        if (new_position.x < 0 || new_position.x >= new_gs.width) {
-            killPlayer(new_gs, i);
+        if (new_gs.players[i].position.x < 0 || new_gs.players[i].position.x >= new_gs.width) {
+            new_gs = kill_player(new_gs, i);
         }
-        if (new_position.y < 0 || new_position.y >= new_gs.height) {
-            killPlayer(new_gs, i);
+        if (new_gs.players[i].position.y < 0 || new_gs.players[i].position.y >= new_gs.height) {
+            new_gs = kill_player(new_gs, i);
         }
         
         // nezabili sme prave niekoho?
-        block curr = gs.blocks[gs.block_index(new_position)];
+        block curr = gs.blocks[gs.block_index(new_gs.players[i].position)];
         if (curr.crossed_by != -1) {
             if (curr.owned_by != curr.crossed_by) {
-                killPlayer(new_gs, curr.crossed_by);
+                kill_player(new_gs, curr.crossed_by);
             }
         }
     }
     
     // kazdy hrac obsadi svoje policko
     for (unsigned i = 0; i < new_gs.players.size(); i++) {
-        if (!new_gs.players[i].alive) {
-            continue;
-        }
-        point position = new_gs.players[i].position;
-        new_gs.blocks[new_gs.block_index(position)].crossed_by = i;
+        if (!new_gs.players[i].alive) continue;
+	int pos_index = new_gs.block_index(new_gs.players[i].position);
+        new_gs.blocks[pos_index].crossed_by = i;
+    }
+
+    // zistime, ci hrac neobsadil uzemie
+    for (unsigned i = 0; i < new_gs.players.size(); i++) {
+	if (!new_gs.players[i].alive) continue;
+	int pos_index = new_gs.block_index(new_gs.players[i].position);
+	if (new_gs.blocks[pos_index].owned_by == (int)i) {
+	    new_gs = own_territory(new_gs, (int)i);
+	}
     }
 
     /*
